82d05c6749aae35014b83564de7fb4c7efe398dd
DragonGames85 <dragongames85@mail.ru>
2023-04-23 13:20:23 +0700

    add Unit Tests

--- src/topKFrequent/topKFrequent.js
+++ src/topKFrequent/topKFrequent.js
@@ -1,4 +1,28 @@
+// Задание:
+// Дан целочисленный массив nums и целое число k.
+// Верните k наиболее часто встречающихся элементов.
+// Вы можете вернуть ответ в любом порядке.
+
+// Ограничения:
+// 1) 1 <= nums.length <= 105
+// 2) -104 <= nums[i] <= 104
+// 3) k is in the range [1, the number of unique elements in the array].
+
+// Ссылка: https://leetcode.com/problems/top-k-frequent-elements/
+
 var topKFrequent = function (nums, k) {
+  if (
+    k <= 0 ||
+    Array.isArray(nums) == false ||
+    k > new Set(nums).size ||
+    !Number.isInteger(k) ||
+    !nums.length ||
+    nums.length > 105 ||
+    nums.some(num => num < -104 || num > 104) ||
+    nums.some(num => !Number.isInteger(num))
+  )
+    return [];
+
   const freqMap = new Map();
   const bucket = [];
   const result = [];
